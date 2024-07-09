4f34804d43d69c7d4b24ef1d0900721ad8f50a06
EngrMunir <smsirajulmonir@gmail.com>
2024-07-08 17:37:59 -0700

    navbar added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,59 +1,58 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import { IoCartOutline } from "react-icons/io5";
+import { MdOutlineSearch } from "react-icons/md";
 
 const Navbar = () => {
+    const navItems =[
+        {
+            title:"Home",
+            path:"/"
+        },
+        {
+            title:"About",
+            path:"/about"
+        },
+        {
+            title:"Services",
+            path:"/services"
+        },
+        {
+            title:"Blog",
+            path:"/blog"
+        },
+        {
+            title:"Contacts",
+            path:"/contacts"
+        }
+    ]
     return (
-        <div className="navbar bg-base-100 text-slate-900">
+       <div className=' bg-base-100 text-slate-900'>
+         <div className="navbar container mx-auto">
   <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <Link href="/">
+        <Image alt='logo' src="/assets/logo.svg" height={60} width={100}/>
+    </Link>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
+    <div className='flex items-center space-x-6'>
+        {
+            navItems.map((item) => (
+                <Link className='font-semibold hover:text-primary duration-300' href={item.path} key={item.path}>{item.title}</Link>
+            ))
+        }
+    </div>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <div className='flex space-x-3 items-center text-xl'>
+    <IoCartOutline />
+    <MdOutlineSearch />
+    <a className="btn btn-outline btn-primary">Appointment</a>
+    </div>
   </div>
 </div>
+       </div>
     );
 };
 

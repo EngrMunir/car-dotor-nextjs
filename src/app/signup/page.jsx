28f954bd07b50183f5671f752f051d0c03c31960
munir <smsirajulmonir@gmail.com>
2024-07-13 10:38:21 -0700

    mongodb connected

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,4 +1,4 @@
-"ude client"
+"use client"
 
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,8 +6,16 @@ import React from 'react';
 import { FaGithub, FaGoogle } from "react-icons/fa6";
 
 const page = () => {
-    const handleRegister = async()=>{
+    const handleRegister = async(event)=>{
 
+        event.preventDefault();
+        
+        const newUser = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            password: event.target.password.value,
+        }
+        console.log(newUser);
     }
     return (
         <div className='container mx-auto py-24 px-24'>
@@ -18,13 +26,16 @@ const page = () => {
             </div>
             <div className='border-2 p-12'>
             <h3 className='text-3xl font-semibold text-primary text-center mb-12'>Sign In</h3>
-                <form onSubmit={handleLogin}>
-                <label htmlFor="email">Email</label>
-                <input type="text" name='email' placeholder='your email' className='input input-bordered w-full max-e-x5 mt-2' />
-                <br /><br />
-                <label htmlFor="email">Password</label>
-                <input type="text" name='password' placeholder='your password' className='input input-bordered w-full max-e-x5 mt-2' />
-                <button type='submit' className='btn btn-primary w-full mt-12'>Sign In</button>
+                <form onSubmit={handleRegister}>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name='name' placeholder='your name' className='input input-bordered w-full max-e-x5 mt-2' />
+                    <br /><br />
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name='email' placeholder='your email' className='input input-bordered w-full max-e-x5 mt-2' />
+                    <br /><br />
+                    <label htmlFor="email">Password</label>
+                    <input type="text" name='password' placeholder='your password' className='input input-bordered w-full max-e-x5 mt-2' />
+                    <button type='submit' className='btn btn-primary w-full mt-12'>Sign In</button>
                 </form>
                 <div>
                     <h6 className='my-12 text-center'>or sign in with</h6>
